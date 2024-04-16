d6f379b7365d92c5dea3c304563b9aada03fd10a
Carlos Enrique Luna Garcia <57882278+carlosLunaGarca@users.noreply.github.com>
2024-04-16 16:22:51 -0500

    init commit

---
+++ prueba_tecnica_linktic/src/main/java/com/example/prueba_tecnica_linktic/PruebaTecnicaLinkticApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_tecnica_linktic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaLinkticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaTecnicaLinkticApplication.class, args);
+    }
+
+}
---
+++ prueba_tecnica_linktic/src/test/java/com/example/prueba_tecnica_linktic/PruebaTecnicaLinkticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_tecnica_linktic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaLinkticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
